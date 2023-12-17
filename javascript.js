4c91ea532e36f382c10b4426648187b8d73e9935
Yan Kolev <y.kolev11@gmail.com>
2023-12-17 19:06:09 +0200

    variables

--- javascript.js
+++ javascript.js
@@ -1,1 +1,5 @@
-alert(2 + 2 + '1' );
+let name = 'John';// string litteral
+let age = '45';//number literal
+let isApproved = false;//boolean literal
+let fistName = undefined;
+let selectColor = null;
