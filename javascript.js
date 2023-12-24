1d4eb3ce9b2cc3de0c3898385a64cfaabe105df1
Yan Kolev <y.kolev11@gmail.com>
2023-12-24 14:05:17 +0200

    updt

--- javascript.js
+++ javascript.js
@@ -1,7 +1,3 @@
-const song = "Fight the Youth";
-const score = 9;
-const highestScore = 10;
-const output = `I like the song ${song}. I gave it a score of ${
-  (score / highestScore) * 100
-}%.`;
-console.log(output);
+const myString = "123";
+const myNum = Number(myString);
+console.log(typeof myNum);
