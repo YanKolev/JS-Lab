c04774f82586783f5a424f701e1dbdb742c9f946
Yan Kolev <y.kolev11@gmail.com>
2023-12-24 13:33:14 +0200

    concat exv2

--- javascript.js
+++ javascript.js
@@ -1,5 +1,7 @@
 const song = "Fight the Youth";
 const score = 9;
 const highestScore = 10;
-const output = `I like the song ${song}. I gave it a score of ${score / highestScore} * 100 }%`;
+const output = `I like the song ${song}. I gave it a score of ${
+  (score / highestScore) * 100
+}%.`;
 console.log(output);
