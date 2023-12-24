e451d96f806c0790accf10220bbf30adea44a8fb
Yan Kolev <y.kolev11@gmail.com>
2023-12-24 13:31:35 +0200

    concat ex

--- javascript.js
+++ javascript.js
@@ -1,9 +1,5 @@
-const button = document.querySelector("button");
-
-function greet() {
-  const name = prompt("What is your name?");
-  const greeting = document.querySelector("#greeting");
-  greeting.textContent = `Hello ${name}, nice to see you!`;
-}
-
-button.addEventListener("click", greet);
+const song = "Fight the Youth";
+const score = 9;
+const highestScore = 10;
+const output = `I like the song ${song}. I gave it a score of ${score / highestScore} * 100 }%`;
+console.log(output);
