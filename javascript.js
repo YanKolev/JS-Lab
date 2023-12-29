6ada2d7e36a6589475d14f1d88209053d56a7c46
Yan Kolev <y.kolev11@gmail.com>
2023-12-29 13:22:51 +0200

    JS updates

--- javascript.js
+++ javascript.js
@@ -1,13 +1,10 @@
-let a = 0;
-alert( Boolean(a) );
-let b = "0";
-alert( Boolean(b) );
-
-alert(a == b);
-//5 > 4 → true
-//"apple" > "pineapple" → false
-//"2" > "12" → true
-//undefined == null → true
-//undefined === null → false
-//null == "\n0\n" → false
-//null === +"\n0\n" → false
+const a = "a";
+const b = "b";
+if (a < b) {
+  // true
+  console.log(`${a} is less than ${b}`);
+} else if (a > b) {
+  console.log(`${a} is greater than ${b}`);
+} else {
+  console.log(`${a} and ${b} are equal.`);
+}
