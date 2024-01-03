fbff0e039ada918afd2bdbe240400a55019d1470
Yan Kolev <y.kolev11@gmail.com>
2024-01-03 15:22:53 +0200

    Login ifelse loop

--- javascript.js
+++ javascript.js
@@ -1,24 +1,53 @@
-if (condition) {
-    //  block of code to be executed if the condition is true
-  }
+//Which of these alerts are going to execute?
 
-  if (condition) {
-    //  block of code to be executed if the condition is true
+//What will the results of the expressions be inside if(...)?
+
+if (-1 || 0) alert( 'first' );
+if (-1 && 0) alert( 'second' );
+if (null || -1 && 1) alert( 'third' );
+
+//the first and the third will execute.
+
+// Runs.
+// The result of -1 || 0 = -1, truthy
+if (-1 || 0) alert( 'first' );
+
+// Doesn't run
+// -1 && 0 = 0, falsy
+if (-1 && 0) alert( 'second' );
+
+// Executes
+// Operator && has a higher precedence than ||
+// so -1 && 1 executes first, giving us the chain:
+// null || -1 && 1  ->  null || 1  ->  1
+if (null || -1 && 1) alert( 'third' );
+
+//Write the code which asks for a login with prompt.
+
+//If the visitor enters "Admin", then prompt for a password, if the input is an empty line or Esc – show “Canceled”, if it’s another string – then show “I don’t know you”.
+
+//The password is checked as follows:
+
+//If it equals “TheMaster”, then show “Welcome!”,
+//Another string – show “Wrong password”,
+//For an empty string or cancelled input, show “Canceled”
+
+let userName = prompt("Who's there?", '');
+
+if (userName === 'Admin') {
+
+  let pass = prompt('Password?', '');
+
+  if (pass === 'TheMaster') {
+    alert( 'Welcome!' );
+  } else if (pass === '' || pass === null) {
+    alert( 'Canceled' );
   } else {
-    //  block of code to be executed if the condition is false
+    alert( 'Wrong password' );
   }
 
-
-  if (condition1) {
-    //  block of code to be executed if condition1 is true
-  } else if (condition2) {
-    //  block of code to be executed if the condition1 is false and condition2 is true
-  } else {
-    //  block of code to be executed if the condition1 is false and condition2 is false
-  }
-
- 
-// -Use if to specify a block of code to be executed, if a specified condition is true
-// -Use else to specify a block of code to be executed, if the same condition is false
-// -Use else if to specify a new condition to test, if the first condition is false
-// -Use switch to specify many alternative blocks of code to be executed
+} else if (userName === '' || userName === null) {
+  alert( 'Canceled' );
+} else {
+  alert( "I don't know you" );
+}
