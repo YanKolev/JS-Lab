f62812fa3d811db78f3ecbb87d588f34f274e7d9
Yan Kolev <y.kolev11@gmail.com>
2024-01-04 15:08:33 +0200

    JS notation added

--- javascript.js
+++ javascript.js
@@ -22,15 +22,17 @@ if (-1 && 0) alert( 'second' );
 // null || -1 && 1  ->  null || 1  ->  1
 if (null || -1 && 1) alert( 'third' );
 
-//Write the code which asks for a login with prompt.
+/**Write the code which asks for a login with prompt.
 
-//If the visitor enters "Admin", then prompt for a password, if the input is an empty line or Esc – show “Canceled”, if it’s another string – then show “I don’t know you”.
+//If the visitor enters "Admin", then prompt for a password, 
+if the input is an empty line or Esc – show “Canceled”, 
+if it’s another string – then show “I don’t know you”.
 
 //The password is checked as follows:
 
 //If it equals “TheMaster”, then show “Welcome!”,
 //Another string – show “Wrong password”,
-//For an empty string or cancelled input, show “Canceled”
+//For an empty string or cancelled input, show “Canceled”/**/
 
 let userName = prompt("Who's there?", '');
 
