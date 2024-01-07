6c227c8098ffb5807f112ff1d2b42561ce9897aa
Yan Kolev <y.kolev11@gmail.com>
2024-01-07 16:09:38 +0200

    JS FNv2

--- javascript.js
+++ javascript.js
@@ -90,3 +90,10 @@ function draw() {
     ctx.fill();
   }
 }
+
+const myText = "The weather is cold";
+const newString = myText.replace("cold", "warm");
+console.log(newString); // Should print "The weather is warm"
+// the replace() string function takes a string,
+// replaces one substring with another, and returns
+// a new string with the replacement made
