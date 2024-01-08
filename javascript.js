2f6f7420cd7df199e97e45b42b4e2c0240ba3e82
Yan Kolev <y.kolev11@gmail.com>
2024-01-08 08:50:47 +0200

    Functions update

--- javascript.js
+++ javascript.js
@@ -1,99 +1,69 @@
-//Which of these alerts are going to execute?
+/*const myArray = ["I", "love", "chocolate","frogs"];
+const madeAstring = myArray.join(" ");
+console.log(madeAstring);*/
+//function to change Array into a string
 
-//What will the results of the expressions be inside if(...)?
+/*const myNumber = Math.random();
+console.log(myNumber);*/
+//function to log random number
 
-if (-1 || 0) alert( 'first' );
-if (-1 && 0) alert( 'second' );
-if (null || -1 && 1) alert( 'third' );
+/*const myArray = ["I", "love", "chocolate","frogs"];
+const madeAstring = myArray.join(" ");
+console.log(madeAstring);
 
-//the first and the third will execute.
+const madeAnotherString = myArray.join();
+console.log(madeAnotherString);*/
+//Strings with optional parameters
 
-// Runs.
-// The result of -1 || 0 = -1, truthy
-if (-1 || 0) alert( 'first' );
-
-// Doesn't run
-// -1 && 0 = 0, falsy
-if (-1 && 0) alert( 'second' );
-
-// Executes
-// Operator && has a higher precedence than ||
-// so -1 && 1 executes first, giving us the chain:
-// null || -1 && 1  ->  null || 1  ->  1
-if (null || -1 && 1) alert( 'third' );
-
-/**Write the code which asks for a login with prompt.
-
-//If the visitor enters "Admin", then prompt for a password, 
-if the input is an empty line or Esc – show “Canceled”, 
-if it’s another string – then show “I don’t know you”.
-
-//The password is checked as follows:
-
-//If it equals “TheMaster”, then show “Welcome!”,
-//Another string – show “Wrong password”,
-//For an empty string or cancelled input, show “Canceled”/**/
-
-let userName = prompt("Who's there?", '');
-
-if (userName === 'Admin') {
-
-  let pass = prompt('Password?', '');
-
-  if (pass === 'TheMaster') {
-    alert( 'Welcome!' );
-  } else if (pass === '' || pass === null) {
-    alert( 'Canceled' );
-  } else {
-    alert( 'Wrong password' );
-  }
-
-} else if (userName === '' || userName === null) {
-  alert( 'Canceled' );
-} else {
-  alert( "I don't know you" );
+/*function hello(name = "Chris"){
+  console.log(`Hello ${name}!`);
 }
-/* Essential concept in coding is functions, 
-which allow you to store a piece of code that does a single task inside a defined block,
-and then call that code whenever you need it using a single short command
- — rather than having to type out the same code multiple times.*/
+hello("Ari");
+hello();*/
+//function that specifies default values by adding =
 
-//manipulation of text string
-const myText = "I am a string";
-const newString = myText.replace("string", "sausage");
-console.log(newString);
-// the replace() string function takes a source string,
-// and a target string and replaces the source string,
-// with the target string, and returns the newly formed string
+//Nameless function can be used to receive another function
+//as a parameter, ex:
+/*
+(function(){
+  alert("Hello");
+});*/
 
-//manipulation of array
-const myArray = ["I", "love", "chocolate", "frogs"];
-const madeAString = myArray.join(" ");
-console.log(madeAString);
-// the join() function takes an array, joins
-// all the array items together into a single
-// string, and returns this new string
+//Run code when user types into a text box- addEventListener()
+//this function expects 2 parameters:
+//name of the event to listen for(ex-keydown), function to run when the event happens
 
-//random number generation
-const myNumber = Math.random();
-// the random() function generates a random number between
-// 0 and up to but not including 1, and returns that number
+/*ORIGINAL FUNCTION: 
 
-//This function draws 100 random circles inside a <canvas> element.
-
-function draw() {
-  ctx.clearRect(0, 0, WIDTH, HEIGHT);
-  for (let i = 0; i < 100; i++) {
-    ctx.beginPath();
-    ctx.fillStyle = "rgb(255 0 0 / 50%)";
-    ctx.arc(random(WIDTH), random(HEIGHT), random(50), 0, 2 * Math.PI);
-    ctx.fill();
-  }
+function logKey(event){
+  console.log (`You pressed "$"{event.key}".`);
 }
+textBox.addEventListener("keydown", logKey);*/
 
-const myText = "The weather is cold";
-const newString = myText.replace("cold", "warm");
-console.log(newString); // Should print "The weather is warm"
-// the replace() string function takes a string,
-// replaces one substring with another, and returns
-// a new string with the replacement made
+/*ANONYMOUS FUNCTION VARIATION:
+textBox.addEventListener("keydown", function(event){
+  console.log(`You pressed "${event.key}".`);
+});*/
+
+//ARROW FUNCTIONS
+/*textBox.addEventListener("keydown",(event)=>{
+  console.log(`You pressed "${event.key}".`);
+});*/
+
+//function containing one return statemet->can implicitly return the expression
+/*const originals = [1, 2, 3];
+const doubled = originals.map(item => item * 2);
+console.log(doubled);*/
+
+/*Map function basically does this: 
+
+function doubleItem(item){
+  return item * 2;
+}*/
+
+const textBox = document.querySelector("#textBox");
+const output = document.querySelector("#output");
+
+textBox.addEventListener("keydown", (event) => {
+  output.textContent = `You pressed "${event.key}".`;
+});
