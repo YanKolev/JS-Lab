fef546e2a93b3eb83c5dffc2728c77a00a9f345e
Yan Kolev <y.kolev11@gmail.com>
2024-01-08 09:13:24 +0200

    FN updates

--- javascript.js
+++ javascript.js
@@ -61,9 +61,10 @@ function doubleItem(item){
   return item * 2;
 }*/
 
-const textBox = document.querySelector("#textBox");
+//Function exercise
+/*const textBox = document.querySelector("#textBox");
 const output = document.querySelector("#output");
 
 textBox.addEventListener("keydown", (event) => {
   output.textContent = `You pressed "${event.key}".`;
-});
+});*/
