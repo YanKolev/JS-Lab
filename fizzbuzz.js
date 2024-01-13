9dcb7d15cc8cd092d1eaf2d4f30f6e66d5391198
Yan Kolev <y.kolev11@gmail.com>
2024-01-13 12:36:51 +0200

    adding MDN exercise

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -58,3 +58,14 @@ for (let i = 1; i <= answer; i++) {
     }
   }
 
+/*
+The first condition now checks if i is divisible by 3 and 5 instead of checking if i is just divisible by 3. 
+We’ve had to do this because if we kept it the way it was, it would run the first condition if (i % 3 === 0), 
+so that if i was divisible by 3, it would print Fizz and then move on to the next number in the iteration, 
+even if i was divisible by 5 as well.
+
+With the condition if (i % 3 === 0 && i % 5 === 0) coming first, 
+we check that i is divisible by both 3 and 5 before moving on to check if
+ it is divisible by 3 or 5 individually in the else if conditions.*/
+
+
