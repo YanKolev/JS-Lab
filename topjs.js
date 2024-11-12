b906e7f38786c99f4763baa83bb0168a370cf73c
Yan Kolev <y.kolev11@gmail.com>
2024-11-12 16:16:11 +0100

    next exercise

--- topjs.js
+++ topjs.js
@@ -29,3 +29,19 @@ const reverseString = function (string) {
 
 // Strings in JavaScript cannot be reversed directly so you're going to have to split it into something else first..
 // do the reversal and then join it back together into a string.
+
+////////////////////////////////////////////////////////////////////
+
+// Exercise 04 - removeFromArray
+// Implement a function that takes an array and some other arguments then removes the other arguments from that array:
+
+// removeFromArray([1, 2, 3, 4], 3); // should remove 3 and return [1,2,4]
+// Hints
+// The first test on this one is fairly easy, but there are a few things to think about (or google) here for the later tests:
+
+// you can manipulate the original array you pass into the function call or create a new array that is returned as the result.
+// how to remove a single element from an array
+// how to deal with multiple optional arguments in a JavaScript function
+// For more information, check out MDN's page on function arguments - scroll down to the bit about Array.from or the spread operator. You can also check out MDN's page on rest parameters.
+
+const removeFromArray = function () {};
