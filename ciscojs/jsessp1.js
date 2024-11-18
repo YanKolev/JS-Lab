f6e31056cfd5c4c10e03364a7e56254b22bcc8ad
Yan Kolev <y.kolev11@gmail.com>
2024-11-18 15:55:55 +0100

    type conversion theory

--- ciscojs/jsessp1.js
+++ ciscojs/jsessp1.js
@@ -58,3 +58,87 @@ console.log(weight); // -> 70
 let rosePrice = 8;
 let rosequantity = 70;
 let roseValue = rosePrice * rosequantity;
+
+//Question 1: Write a code that will create variables and initialize them with values of Boolean, Number, BigInt, String, and undefined types using (when possible) literals and constructor functions.
+console.log(Boolean('string'));
+console.log(Number(42));
+console.log(BigInt(42n));
+console.log(String('text'));
+
+let isDataValid = true;
+console.log(typeof isDataValid);
+
+let firstYear = 2024;
+console.log(typeof firstYear);
+
+let big = 123123000000n;
+console.log(typeof big);
+
+let name1 = 'Caitlyn';
+console.log(typeof name1);
+
+let x = undefined;
+
+////////////////////////////////////// course answers
+let b1 = true;
+let b2 = Boolean(true);
+
+let n1 = 100;
+let n2 = Number(200);
+
+let bi1 = 100n;
+let bi2 = BigInt(200);
+
+let s1 = 'Hello';
+let s2 = String('Hello');
+
+let u1 = undefined;
+
+//Question 2: Print all values and all types of those values using console.log. Try to use string interpolation to display the value and type at the same time with a single console.log call, e.g. in the following form: 1000 [number].
+console.log(b1);
+console.log(`${b1}, [${typeof b1}]`);
+console.log(`${n1}, [${typeof n1}]`);
+console.log(`${bi1}, [${typeof bi1}]`);
+console.log(`${s1}, [${typeof s1}]`);
+console.log(`${u1}, [${typeof u1}]`);
+
+//Question 3: Carry out a chain of conversions: create a Boolean from a BigInt created from a Number that was created from a String. Start with the value "1234". Is it possible?
+
+let start = '1234';
+let startStr = String(start);
+let startNum = Numnber(startStr);
+let startBig = BigInt(startNum);
+let startBoo = Boolean(startBig);
+
+console.log(`${startBoo} [${typeof startBoo}]`); //forgot to add
+
+// version 2 //course provided
+let b = Boolean(BigInt(Number('1234')));
+console.log(`${b} [${typeof b}]`);
+
+//Question 4: Try adding two values of the same type and check the result type. Try it for all primitive types.
+
+// const wa = 'test';
+// const sm = 'text';
+// console.log(wa + sm);
+
+let b0 = true + false;
+let n = 100 + 200;
+let bi = 100n + 200n;
+let s = 'he' + 'llo';
+let u = undefined + undefined;
+
+console.log(`${b0} [${typeof b0}]`); // !! number
+console.log(`${n} [${typeof n}]`);
+console.log(`${bi} [${typeof bi}]`);
+console.log(`${s} [${typeof s}]`);
+console.log(`${u} [${typeof u}]`); // !!! number
+
+//Question 5: Try adding two values of different types and check the results.
+let sumV = 100 + false;
+console.log(`${sumV} [${typeof sumV}]`);
+//Question 6: Try to modify the line const str1 = 42 + "1"; to get the result 43 (without removing the quotes around 1).
+const str1 = 42 + '1';
+//const str1 = 42 + Number('1');
+console.log(str1);
+//Course variant = const str = 42 + + "1";
