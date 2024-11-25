6898619bfca47ec94c850253ebc8e33403df5952
Yan Kolev <y.kolev11@gmail.com>
2024-11-25 14:31:47 +0100

    Control flow theory

--- ciscojs/jsessp1.js
+++ ciscojs/jsessp1.js
@@ -304,3 +304,17 @@ books.push({
 // let n = false || 'false';
 
 // console.log(n);
+
+//////////////////////////////////////////////////////////////////////
+//Control Flow (if statements and loops)
+///////////////////////////////////////////////////////////////////////////
+
+// Exercise 1: Write a script that will ask the user to input a number.
+
+// Display the message "Bingo!" when the number is greater than 90 but less than 110, otherwise display the message: "Miss". Use the if statement.
+
+// Exercise 2: Rewrite the code from the previous task, replacing if with a ternary conditional operator.
+
+// Exercise 3: Write a simple calculator application. Ask the user for the following input, one by one: two numbers and a character representing a mathematical operation, one of "+", "-", "*", "/". If the user input is valid, calculate the result and show it to the user. If the user input is invalid, display a message that informs the user that an error has occurred.
+
+// Remember that the value returned by the prompt function is of the type string. You can use the Number.isNaN method to check if you get the correct number after conversion. For example, calling Number.isNaN(10) will return false, while Number.isNaN(NaN) will return true.
