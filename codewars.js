3c56a8442edb9f855cf5f748a04e9eb2dcb6598e
Yan Kolev <y.kolev11@gmail.com>
2024-12-21 15:00:48 +0100

    codewars notepad

---
+++ codewars.js
@@ -0,0 +1,37 @@
+
+// Complete the function howManydays. 
+// It accepts 1 parameter month, which means the month of the year. 
+// Different months have a different number of days as shown in the table below. 
+// Return the number of days that are in month. 
+// There is no need for input validation: month will always be greater than 0 and less than or equal to 12.
+
+// +---------------+-------------+
+// |    month      |    days     |
+// +---------------+-------------+
+// |1,3,5,7,8,10,12|     31      |
+// +---------------+-------------+
+// |4,6,9,11       |     30      |
+// +---------------+-------------+
+// |2              |     28      |  (Do not consider the leap year)
+// +---------------+-------------+
+
+
+function howManydays(month){
+  var days;
+  switch (){
+      case 0: 
+      //code block
+      break;
+      case 1:
+      //code block
+      break;
+      case 2:
+      //code block
+      break;
+      
+      default:
+      //code block
+  
+  }
+  return days;
+}
