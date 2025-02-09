5941772dad43f92aefaed600c8a6d203e5d9a74b
Yan Kolev <y.kolev11@gmail.com>
2025-02-09 17:57:33 +0100

    final refactoring

--- topfiles/shopping-script.js
+++ topfiles/shopping-script.js
@@ -1,39 +1,3 @@
-// const list = document.querySelector('ul');
-
-// const input = document.querySelector('input');
-
-// const btn = document.querySelector('#btn');
-
-// function responseF() {
-//   let currentValue = document.getElementById('item');
-//   // currentValue.innerText = console.log(`Input is ${input.value}`);
-//   currentValue.innerText = '';
-
-//   const newList = document.createElement('li');
-//   const span = document.createElement('span');
-//   const newBtn = document.createElement('button');
-
-//   newList.appendChild(span);
-//   newList.appendChild(newBtn);
-
-//   span.textContent = currentValue.value;
-//   newBtn.textContent = 'Delete';
-
-//   list.appendChild(newList);
-
-//   // newBtn.addEventListener('onclick', function (e) {
-//   //   // const newList = document.getElementsByName('li');
-//   //   list.removeChild(list.firstElementChild); // try with instead of list.first el child - just to newList
-//   // });
-
-//   newBtn.addEventListener('click', removeList);
-
-//   function removeList() {
-//     list.remove(document.querySelector('li')); here also newList so it targets the element, Needs rework
-//   }
-// }
-
-//other variation
 const list = document.querySelector('ul');
 const input = document.querySelector('input');
 const button = document.querySelector('button');
@@ -44,15 +8,15 @@ button.addEventListener('click', () => {
 
   const listItem = document.createElement('li');
   const listText = document.createElement('span');
-  const listBnt = document.createElement('buton');
+  const listBtn = document.createElement('button');
 
   listItem.appendChild(listText);
-  listText.textContent = myItem; //missed this
-  listItem.appendChild(listBnt);
-  listBnt.textContent = 'Delete';
-  list.appendChild(listItem); // this also,
+  listText.textContent = myItem;
+  listItem.appendChild(listBtn);
+  listBtn.textContent = 'Delete';
+  list.appendChild(listItem);
 
-  listBnt.addEventListener('click', () => {
+  listBtn.addEventListener('click', () => {
     list.removeChild(listItem);
   });
 
