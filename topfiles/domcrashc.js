7cf4b5fd1991810564dca7c196cfde0c4f4b5280
Yan Kolev <y.kolev11@gmail.com>
2025-02-12 10:41:09 +0100

    Traversing the DOM

--- topfiles/domcrashc.js
+++ topfiles/domcrashc.js
@@ -121,3 +121,89 @@ for (let i = 0; i < odd.length; i++) {
   odd[i].style.backgroundColor = 'grey';
   even[i].style.backgroundColor = 'darkgrey';
 }
+
+//TRAVERSING THE DOM
+
+const itemList = document.querySelector('#items');
+//parentNode propery
+// console.log(itemList.parentNode);
+
+// itemList.parentNode.style.backgroundColor = 'lightgrey';
+// console.log(itemList.parentNode.parentNode); // its possible to chain them and display the parent of the parentNode
+
+//using parentelement
+// console.log(itemList.parentElement);
+// itemList.parentElement.style.backgroundColor = 'pink';
+// console.log(itemList.parentElement.parentElement.parentElement);
+
+//most of the time parentNode and ParentElement can be interchangable
+
+//childnodes
+
+// console.log(itemList.childNodes); //returns a nodelist basically an array
+// // at position 0 its text node- it represents whitespace/ linebreak
+
+// //children
+// console.log(itemList.children); //it will return just the notes, it disregards the linebreaks a bit more cleaner array
+// //it returns HTML collection- NOT A NODE LIST
+
+// console.log(itemList.children[1]); // we can acess the children like that / same array index starting from 0
+
+// //firstchild
+// console.log(itemList.firstChild); // will return text node- it will return the whitespace/linebreak
+//not so used
+
+//more used
+//firstElementChild
+// console.log(itemList.firstElementChild);
+// itemList.firstElementChild.textContent = 'Hello 1';
+
+// //LAST CHILD = SAME AS FIRST CHILD
+
+// //LASTELEMENTCHILD = SAME AS FIRSTELEMENTCHILD
+
+// //nextSibling
+// console.log(itemList.nextSibling); // returns text node
+
+// //nextElementSibling
+// console.log(itemList.nextElementSibling);
+
+// //previousSibling
+// console.log(itemList.previousSibling); //returns text node
+
+//previousElementSibling
+// console.log(itemList.previousElementSibling);
+// itemList.previousElementSibling.style.color = 'green';
+
+////////////////////////////////////////////////////////////////////
+//CREATING ELEMENTS AND INSERTING THEM
+
+//create a div
+
+const newDiv = document.createElement('div');
+// console.log(newDiv);
+
+//Add Class
+newDiv.className = 'hello';
+
+//Add id
+newDiv.id = 'hello1';
+
+//add attribute
+newDiv.setAttribute('title', 'Hello div');
+
+// to insert information we need to create a text node
+const newDivText = document.createTextNode('Hello World');
+//then we need to add the txt to div with appendchild
+newDiv.appendChild(newDivText);
+
+//to insert information in the html
+const container = document.querySelector('header .container');
+const h1 = document.querySelector('header h1');
+
+console.log(newDiv);
+
+newDiv.style.fontSize = '30px'; // it can be treated like any other DOM element
+
+//needs 2 parameters- the one element that we want to put in and the element BEFORE we want to put
+container.insertBefore(newDiv, h1);
